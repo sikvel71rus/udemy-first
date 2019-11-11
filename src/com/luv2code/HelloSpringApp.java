55d1a649026e32e1fe03f2ede980b8ff381b3251
Никита <jubin2la@mail.ru>
2019-11-12 01:41:38 +0300

    add files

--- src/com/luv2code/HelloSpringApp.java
+++ src/com/luv2code/HelloSpringApp.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpringApp {
     public static void main(String[] args) {
+
         //load the spring configuration file
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
